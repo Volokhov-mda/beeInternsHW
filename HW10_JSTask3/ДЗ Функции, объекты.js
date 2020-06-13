a5592e3f13555b6205413653c4e1caf471fa06b7
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-06-13 18:35:22 +0300

    init

---
+++ HW10_JSTask3/ДЗ Функции, объекты.js
@@ -0,0 +1,47 @@
+// Домашнее задание:
+// Задание 1: Функция, возвращающая объект, и меняющая местами ключ <-> значение.
+// исходный объект должен остаться неизменным.
+// Уровень 1: Работа с конкретныйм объектом и конкретными его свойствами
+
+const firstObject = {
+  'one': 'number',
+};
+
+function resolve1(inputObject) {
+ // body
+};
+
+const result1 = resolve1(firstObject);
+console.log(result1); // ожидаемый результат { 'number': 'one' }
+console.log(firstObject); // ожидаемый результат { 'one': 'number' }
+
+
+// Уровень 2: Работа с любым объектом БЕЗ вложенных объектов внутри
+// и любым количеством свойствам
+// Данный объект. Должен остаться неизменным
+const secondObject = {
+  'apple': 'fruit',
+  'potato': 'vegetable',
+  'strawberry': 'berry',
+};
+
+function resolve2(inputObject) {
+ // body
+};
+
+const result2 = resolve2(secondObject);
+console.log(result2); 
+// ожидаемый результат: { 'apple': 'fruit', 'potato': 'vegetable', 'strawberry': 'berry' }
+
+console.log(secondObject);
+// ожидаемый результат { 'fruit': 'apple', 'vegetable': 'potato', 'berry': 'strawberry' }
+
+
+// Задание 2. Написать функцию, возвращающую век, соответствующий данному году
+// Использовать Глобальный объект Math
+
+const year = 1905;
+centuryFromYear(year); // 20
+
+const year2 = 1700;
+centuryFromYear(year); // 17.
