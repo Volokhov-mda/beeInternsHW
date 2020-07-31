6fb68bb36784f6b959921904adf1b06fb3fa6ebd
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-07-31 17:26:58 +0300

    Added webpack-dev-server

--- HW16_WebpackTask/webpack.config.js
+++ HW16_WebpackTask/webpack.config.js
@@ -1,8 +1,21 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './index.js', 
     output: {
         filename: 'bundle.js',
         path: __dirname + '/public'
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    optimization: {
+        moduleIds: 'natural'
+    },
+    devServer: {
+        contentBase: __dirname + '/public'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 }
