d5c597a21ecaa4e6348d10d523542167935c4808
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-07-31 22:18:51 +0300

    Added name option for file loader

--- HW16_WebpackTask/webpack.config.js
+++ HW16_WebpackTask/webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
                 outputPath: 'images',
+                name: '[name].[ext]'
             }
         },
         {
